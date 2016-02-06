6536cb8a14be25234fc42425395c8c9be28c526c
Michał Papierski <michal@papierski.net>
2016-02-06 11:21:38 +0100

    Support for setting parser callbacks.

--- js/api.js
+++ js/api.js
@@ -25,31 +25,34 @@ VTerm.prototype.get_utf8 = function() {
  */
 VTerm.prototype.parser_set_callbacks = function(callbacks) {
   // XXX: sizeof(VTermParserCallbacks) right?
-  this._parser_callbacks = Module._malloc(7 * 4);
+  this._parser_callbacks = Module._malloc(1024);
+  if (!this._parser_callbacks) {
+    throw new Error('Not enough memory');
+  }
   // text
   if (callbacks.text) {
-    Module.setValue(this._parser_callbacks + (0 * 4), Module.Runtime.addFunction(callbacks.text), 'i32');
+    Module.setValue(this._parser_callbacks + (0 * 4), Module.Runtime.addFunction(callbacks.text), 'i32*');
   }
   if (callbacks.control) {
-    Module.setValue(this._parser_callbacks + (1 * 4), Module.Runtime.addFunction(callbacks.control), 'i32');
+    Module.setValue(this._parser_callbacks + (1 * 4), Module.Runtime.addFunction(callbacks.control), 'i32*');
   }
   if (callbacks.escape) {
-    Module.setValue(this._parser_callbacks + (2 * 4), Module.Runtime.addFunction(callbacks.escape), 'i32');
+    Module.setValue(this._parser_callbacks + (2 * 4), Module.Runtime.addFunction(callbacks.escape), 'i32*');
   }
   if (callbacks.csi) {
-    Module.setValue(this._parser_callbacks + (3 * 4), Module.Runtime.addFunction(callbacks.csi), 'i32');
+    Module.setValue(this._parser_callbacks + (3 * 4), Module.Runtime.addFunction(callbacks.csi), 'i32*');
   }
   if (callbacks.osc) {
-    Module.setValue(this._parser_callbacks + (4 * 4), Module.Runtime.addFunction(callbacks.osc), 'i32');
+    Module.setValue(this._parser_callbacks + (4 * 4), Module.Runtime.addFunction(callbacks.osc), 'i32*');
   }
   if (callbacks.dcs) {
-    Module.setValue(this._parser_callbacks + (5 * 4), Module.Runtime.addFunction(callbacks.dcs), 'i32');
+    Module.setValue(this._parser_callbacks + (5 * 4), Module.Runtime.addFunction(callbacks.dcs), 'i32*');
   }
   if (callbacks.resize) {
-    Module.setValue(this._parser_callbacks + (6 * 4), Module.Runtime.addFunction(callbacks.resize), 'i32');
+    Module.setValue(this._parser_callbacks + (6 * 4), Module.Runtime.addFunction(callbacks.resize), 'i32*');
   }
 
-  Module._vterm_parser_set_callbacks(this.term, callbacks, null);
+  Module._vterm_parser_set_callbacks(this.term, this._parser_callbacks);
 };
 
 VTerm.prototype.close = function() {
@@ -58,6 +61,10 @@ VTerm.prototype.close = function() {
   }
   Module._vterm_free(this.term);
   delete this.term;
+  if (!this._parser_callbacks) {
+    return;
+  }
+  Module._free(this._parser_callbacks);
 }
 
 /**
--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
 var assert = require('assert');
+var sinon = require('sinon');
 var VTerm = require('../vterm.js');
 
 describe('VTerm', function() {
   var term = new VTerm(25, 80);
+  after(function() {
+    term.close();
+  });
+
   describe('close', function () {
     it('should not fail when called twice', function () {
       term.close();
@@ -33,17 +38,36 @@ describe('VTerm', function() {
       assert.equal(false, term.get_utf8());
     });
   });
-  describe('parser callbacks', function() {
-    it('set works with an object', function() {
-      term.parser_set_callbacks({
-        text: function() {},
-        control: function() {},
-        escape: function() {},
-        csi: function() {},
-        osc: function() {},
-        dcs: function() {},
-        resize: function() {}
-      });
+  describe.skip('parser', function() {
+    var text_callback = sinon.stub().throws("Error");
+    var control_callback = sinon.stub().throws("Error");
+    var escape_callback = sinon.stub().throws("Error");
+    var csi_callback = sinon.stub().throws("Error");
+    var osc_callback = sinon.stub().throws("Error");
+    var dcs_callback = sinon.stub().throws("Error");
+    var resize_callback = sinon.stub().throws("Error");
+
+    term.set_utf8(true);
+    term.parser_set_callbacks({
+      text: text_callback,
+      control: control_callback,
+      escape: escape_callback,
+      csi: csi_callback,
+      osc: osc_callback,
+      dcs: dcs_callback,
+      resize: resize_callback
+    });
+    it('calls text callback', function() {
+      assert.equal(5, term.write('hello'));
+      assert(
+	text_callback.called ||
+	control_callback.called ||
+	escape_callback.called ||
+	csi_callback.called ||
+	osc_callback.called ||
+	dcs_callback.called ||
+	resize_callback.called
+      );
     });
   });
 });
